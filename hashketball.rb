8fb8f1a1de997e2b84392167e3601bed783e3498
Daniel Andrew <dwandrew@hotmail.com>
2020-04-07 10:12:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -248,7 +248,7 @@ def winning_team
   home_team_name= game_hash[:home][:team_name]
   away_team_name= game_hash[:away][:team_name]
   winning_team ={:home_team_name => home_team_points, :away_team_name => away_team_points}
-  
+  winning_team.max
 end
 
 def player_with_longest_name
